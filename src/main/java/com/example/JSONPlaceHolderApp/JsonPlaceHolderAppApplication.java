15e0861173211ffaecb1dd10de9da7592c3565e5
gregorio-calabretta <gregoriocalabretta94@gmail.com>
2024-05-07 10:39:45 +0200

    first commit

---
+++ src/main/java/com/example/JSONPlaceHolderApp/JsonPlaceHolderAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.JSONPlaceHolderApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonPlaceHolderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonPlaceHolderAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JSONPlaceHolderApp/JsonPlaceHolderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JSONPlaceHolderApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonPlaceHolderAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
